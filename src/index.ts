97c878855561561c2f3ea46cdb6cef5667b0ad14
Nexus Prime <nicolas.estevao.lima1004@hotmail.com>
2022-05-02 20:57:35 -0300

    1.5.1

--- src/index.ts
+++ src/index.ts
@@ -59,52 +59,46 @@ export const EasyEmbed = class<STRING extends string = typeof defaultTypes[numbe
     }
 
     create(type: STRING, textOrEmbedOptions: string | Discord.MessageEmbedOptions, options?: Omit<Discord.InteractionReplyOptions, 'embeds'>) {
-        const selectedType = EasyEmbed.options.types?.find(t => t.name === type);
-        if(!selectedType) throw new Error("This Embed type not exist");
+        const selectedEmbedType = EasyEmbed.options.types?.find(t => t.name === type);
+        if(!selectedEmbedType) throw new Error("This Embed type not exist");
 
-        const { footer, color, author, description, fields, image, thumbnail, timestamp, url } = selectedType;
-        let result: Discord.MessageEmbed = new Discord.MessageEmbed({
-            footer, color, author, description, fields, image, thumbnail, timestamp, url
-        });
+        let result: Discord.MessageEmbedOptions = {}
         
         const selectedSeparator = EasyEmbed.options.separator || defaultOptions.separator;
 
         if (typeof textOrEmbedOptions === 'string') {
-            result.setTitle(`${!selectedType.emoji
-                ? textOrEmbedOptions
-                : `${selectedType.emoji}${selectedSeparator}${textOrEmbedOptions}`
-            }`);
+            if(!selectedEmbedType.emoji) {
+                if(textOrEmbedOptions.length < 1) return;
+
+                result.title = textOrEmbedOptions;
+            } else {
+                result.title = `${selectedEmbedType.emoji}${selectedSeparator}${textOrEmbedOptions}`;
+            }
             
-            result.setColor(selectedType.color);
+            result.color = selectedEmbedType.color;
         } else {
-            result.setTitle(`${!selectedType.emoji
-                ? textOrEmbedOptions
-                : `${selectedType.emoji}${selectedSeparator}${textOrEmbedOptions}`
-            }`);
-            result.setColor(<Discord.ColorResolvable>selectedType?.color);
+            const embed: Discord.MessageEmbedOptions = {
+                author: textOrEmbedOptions.author,
+                color: selectedEmbedType.color,
+                description: textOrEmbedOptions.description,
+                fields: textOrEmbedOptions.fields,
+                footer: textOrEmbedOptions.footer,
+                image: textOrEmbedOptions.image,
+                thumbnail: textOrEmbedOptions.thumbnail,
+                timestamp: textOrEmbedOptions.timestamp,
+                url: textOrEmbedOptions.url,
+                video: textOrEmbedOptions.video,
+            }
+            
+            if(!selectedEmbedType.emoji) {
+                if(!textOrEmbedOptions.title || textOrEmbedOptions.title.length < 1) return;
+                
+                result.title = textOrEmbedOptions.title;
+            } else {
+                result.title = `${selectedEmbedType.emoji}${selectedSeparator}${textOrEmbedOptions}`;
+            }
 
-            textOrEmbedOptions.footer?.text && result.setFooter({ 
-                text: textOrEmbedOptions.footer?.text, 
-                iconURL: textOrEmbedOptions.footer?.iconURL 
-            });
-
-            textOrEmbedOptions.author?.name && result.setAuthor({
-                name: textOrEmbedOptions.author?.name,
-                iconURL: textOrEmbedOptions.author?.iconURL,
-                url: textOrEmbedOptions.author?.url
-            });
-
-            textOrEmbedOptions.description && result.setDescription(textOrEmbedOptions.description);
-
-            textOrEmbedOptions.fields?.forEach(field => result.addField(field.name, field.value, field.inline));
-
-            textOrEmbedOptions.image?.url && result.setImage(textOrEmbedOptions.image?.url);
-
-            textOrEmbedOptions.thumbnail?.url && result.setThumbnail(textOrEmbedOptions.thumbnail?.url);
-
-            textOrEmbedOptions.timestamp && result.setTimestamp(textOrEmbedOptions.timestamp);
-
-            textOrEmbedOptions.url && result.setURL(textOrEmbedOptions.url);
+            result = { ...embed }
         }
     
         return { 
